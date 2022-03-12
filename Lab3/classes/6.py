c9b203b60b73cd72f8e35d0b6d82112c006e2205
Ruchanov <98680173+Ruchanov@users.noreply.github.com>
2022-03-12 16:25:21 +0600

    'lab3'

--- Lab3/classes/6.py
+++ Lab3/classes/6.py
@@ -1,11 +1,5 @@
-def prime(a):
-    res=True
-    if a==2:
-        return res
-    for i in range(2,int(a/2)+1):
-        if a%i==0:
-            res=False
-    return res
 l=list(map(int,input().split()))
-l2=filter(prime,l)
-print(*l2)
+for i in range(len(l)):
+    for j in range(2,int((l[i]/2)+1)):
+        l=filter(lambda x:x==j or x%j,l)
+print(l)
