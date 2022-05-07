8ee556c2ef985c9f94fad89f37cd563175db1b92
Ruchanov <98680173+Ruchanov@users.noreply.github.com>
2022-05-07 15:56:18 +0600

    'lab11'

---
+++ lab11/5.py
@@ -0,0 +1,61 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    host='localhost', 
+    database='postgres',
+    port=5432,
+    user='postgres',
+    password='Ayef1407_'
+)
+
+''' my SQL in DATAGRIP:
+BY NAME:
+create or replace procedure deleteName(first_name varchar, last_name varchar)
+as
+$$
+begin
+    delete
+    from phonebook_lab11 as p
+    where (p.first_name = $1) and(p.last_name = $2);
+end;
+$$
+    LANGUAGE plpgsql;
+
+
+
+---------------------------------5(2)-------------------
+BY NUM:
+create or replace procedure deleteNum(num varchar)
+as
+$$
+begin
+    delete
+    from phonebook_lab11 as p
+    where (p.phone_num = $1) ;
+end;
+$$
+    LANGUAGE plpgsql;
+'''
+
+
+cursor = conn.cursor()
+conn.autocommit = True
+
+first_name = str(input("Delete by First_name: "))
+last_name = str(input("Delete by Last_name: "))
+
+cursor.execute('CALL deleteName(%s,%s)', (first_name, last_name))
+
+cursor.callproc('getStudentNameSurnamePhone', [first_name])
+result = cursor.fetchall()
+print(result)
+
+
+
+num=str(input("Num: "))
+cursor.execute(f'CALL deleteNum(\'{num}\')')
+
+sql = f"select * from phonebook";
+cursor.execute(sql)
+info = cursor.fetchall()
+print(info)
